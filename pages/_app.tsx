57dc927e0f4f034d57eee66cbfee9737874b1348
Coder2195 <Coder2195Mail@gmail.com>
2022-09-27 09:46:46 -0400

    hope this works now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   return <>
     <Head>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -14,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#000000" />
       <meta property="og:site_name" content="Coder2195's Website" />
-      <meta property="og:url" content="https://coder2195.vercel.app" />
+      <meta property="og:url" content={`https://coder2195.vercel.app${router.asPath}`} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="https://coder2195.vercel.app/apple-touch-icon.png" />
     </Head>
