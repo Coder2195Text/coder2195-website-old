5ea23d86108c24513f00630ae1fb98f33556d7c1
Coder2195 <Coder2195Mail@gmail.com>
2022-09-27 09:22:05 -0400

    added og meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
+      <meta property="og:title" content="Coder2195 - Home" />
+      <meta property="og:site_name" content="Coder2195's Website" />
+      <meta property="og:url" content="https://coder2195.vercel.app" />
+      <meta property="og:description" content="Welcome to coder2195's personal website!!! " />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://coder2195.vercel.app/apple-touch-icon.png" />
     </Head>
     <Component {...pageProps} />
   </>
