bbe713e6256cf52257d5ae40db50d5cc278b27da
Coder2195 <Coder2195Mail@gmail.com>
2022-10-05 10:07:19 -0400

    try seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,63 +7,64 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [ready, setReady] = useState(false)
   useEffect(() => {
     const promises: Promise<any>[] = [];
-    document.fonts.forEach((f)=>{
+    document.fonts.forEach((f) => {
       promises.push(f.load())
     })
     Promise.all(promises).then(() => {
       setReady(true)
     })
   }, [])
-  if (!ready) return <></>
-  return <>
-    <DefaultSeo
-      additionalLinkTags={[
+  const seo = <DefaultSeo
+    additionalLinkTags={[
+      {
+        rel: "apple-touch-icon",
+        href: "/apple-touch-icon.png",
+        sizes: "180x180"
+      }, {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      }, {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest"
+      }, {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5"
+      }
+    ]}
+    additionalMetaTags={[
+      {
+        name: "msapplication-TileColor",
+        content: "#da532c"
+      }, {
+        name: "theme-color",
+        content: "#000000"
+      }
+    ]}
+    openGraph={{
+      images: [
         {
-          rel: "apple-touch-icon",
-          href: "/apple-touch-icon.png",
-          sizes: "180x180"
-        }, {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon-32x32.png"
-        }, {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon-16x16.png"
-        },
-        {
-          rel: "manifest",
-          href: "/site.webmanifest"
-        }, {
-          rel: "mask-icon",
-          href: "/safari-pinned-tab.svg",
-          color: "#5bbad5"
+          url: "https://coder2195.vercel.app/android-chrome-384x384.png",
+          width: 384,
+          height: 384,
+          type: "image/png"
         }
-      ]}
-      additionalMetaTags={[
-        {
-          name: "msapplication-TileColor",
-          content: "#da532c"
-        }, {
-          name: "theme-color",
-          content: "#000000"
-        }
-      ]}
-      openGraph={{
-        images: [
-          {
-            url: "https://coder2195.vercel.app/android-chrome-384x384.png",
-            width: 384,
-            height: 384,
-            type: "image/png"
-          }
-        ],
-        site_name: "Coder2195's Website"
-      }}
+      ],
+      site_name: "Coder2195's Website"
+    }}
 
-    />
+  />
+  if (!ready) return <>{seo}</>
+  return <>
+    {seo}
     <Component {...pageProps} />
   </>
 }
